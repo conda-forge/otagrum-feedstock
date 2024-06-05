211ab648f3bf074a5e0658188ffb0521eb9cd2cc
Julien Schueller <schueller@phimeca.com>
2024-06-05 17:35:49 +0200

    Update to 0.10

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-mkdir build && cd build
-
 cmake \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
   -DPython_FIND_STRATEGY=LOCATION \
   -DPython_ROOT_DIR=${PREFIX} \
-  ..
+  -B build -S .
 
-make install -j${CPU_COUNT}
-ctest -R pyinstallcheck --output-on-failure -j${CPU_COUNT} -E "TabuList"
+cmake --build build --target install --parallel ${CPU_COUNT}
+
+if test "$CONDA_BUILD_CROSS_COMPILATION" != "1"
+then
+  ctest --test-dir build -R pyinstallcheck --output-on-failure -j${CPU_COUNT} -E TabuList
+fi
