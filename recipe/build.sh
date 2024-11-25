0cefed8e5e156b97322ec2fddb6c7e318f60295e
Julien Schueller <schueller@phimeca.com>
2024-11-25 16:22:42 +0100

    Update to 0.11

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+# RuntimeError: std::bad_cast on osx in FromPotential
+sed -i'.bak' "s|dynamic_cast|static_cast|g" lib/src/Utils.cxx
+
 cmake ${CMAKE_ARGS} \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_INSTALL_RPATH="${PREFIX}/lib" \
+  -DCMAKE_UNITY_BUILD=ON \
   -DPython_FIND_STRATEGY=LOCATION \
   -DPython_ROOT_DIR=${PREFIX} \
   -B build -S .
